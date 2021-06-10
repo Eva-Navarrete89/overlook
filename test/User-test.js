a0d9b7f94ca5af98d35f9d124ec07011aebdfde9
Eva Navarrete <evanavarrete07@gmail.com>
2021-06-10 13:08:07 -0600

    Add import/exports to test files and classs files

---
+++ test/User-test.js
@@ -0,0 +1,5 @@
+import chai from 'chai';
+const expect = chai.expect;
+import User from '../src/User';
+import Bookings from '../src/Bookings';
+import {customerData, bookingsData} from '.test-data';
