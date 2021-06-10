1192bc77efb56169a1ff4a32580f4306e4d8cfa2
Eva Navarrete <evanavarrete07@gmail.com>
2021-06-10 12:46:20 -0600

    Add dummy data

---
+++ test/test-data.js
@@ -0,0 +1,124 @@
+const customerData = [
+  {
+   "id": 1,
+   "name": "Eva Navarrete"
+ },
+ {
+   "id": 2,
+   "name": "Lindsay Parker"
+ },
+ {
+   "id": 3,
+   "name": "Hannah Hudson"
+ }
+];
+const roomData = [
+  {
+     "number": 1,
+     "roomType": "residential suite",
+     "bidet": true,
+     "bedSize": "queen",
+     "numBeds": 1,
+     "costPerNight": 358.4
+   },
+   {
+     "number": 2,
+     "roomType": "suite",
+     "bidet": false,
+     "bedSize": "full",
+     "numBeds": 2,
+     "costPerNight": 477.38
+   },
+   {
+     "number": 3,
+     "roomType": "single room",
+     "bidet": false,
+     "bedSize": "king",
+     "numBeds": 1,
+     "costPerNight": 491.14
+   },
+   {
+     "number": 4,
+     "roomType": "single room",
+     "bidet": false,
+     "bedSize": "queen",
+     "numBeds": 1,
+     "costPerNight": 429.44
+   },
+   {
+     "number": 5,
+     "roomType": "single room",
+     "bidet": true,
+     "bedSize": "queen",
+     "numBeds": 2,
+     "costPerNight": 340.17
+   }
+];
+
+const bookingsData = [
+  {
+    "id": "5fwrgu4i7k55hl6sz",
+    "userID": 9,
+    "date": "2020/04/22",
+    "roomNumber": 15,
+    "roomServiceCharges": []
+  },
+  {
+    "id": "5fwrgu4i7k55hl6t5",
+    "userID": 43,
+    "date": "2020/01/24",
+    "roomNumber": 24,
+    "roomServiceCharges": []
+  },
+  {
+    "id": "5fwrgu4i7k55hl6t6",
+    "userID": 13,
+    "date": "2020/01/10",
+    "roomNumber": 12,
+    "roomServiceCharges": []
+  },
+  {
+    "id": "5fwrgu4i7k55hl6t7",
+    "userID": 20,
+    "date": "2020/02/16",
+    "roomNumber": 7,
+    "roomServiceCharges": []
+  },
+  {
+    "id": "5fwrgu4i7k55hl6t8",
+    "userID": 1,
+    "date": "2020/02/05",
+    "roomNumber": 12,
+    "roomServiceCharges": []
+  },
+  {
+   "id": "5fwrgu4i7k55hl6t9",
+   "userID": 38,
+   "date": "2020/02/14",
+   "roomNumber": 14,
+   "roomServiceCharges": []
+ },
+ {
+   "id": "5fwrgu4i7k55hl6ta",
+   "userID": 25,
+   "date": "2020/01/11",
+   "roomNumber": 9,
+   "roomServiceCharges": []
+ },
+ {
+   "id": "5fwrgu4i7k55hl6tb",
+   "userID": 49,
+   "date": "2020/02/06",
+   "roomNumber": 5,
+   "roomServiceCharges": []
+ },
+ {
+   "id": "5fwrgu4i7k55hl6tc",
+   "userID": 22,
+   "date": "2020/01/30",
+   "roomNumber": 13,
+   "roomServiceCharges": []
+ },
+];
+
+export {customerData, roomData, bookingsData};
