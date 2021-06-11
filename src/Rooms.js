156ca55d049468a1dbd2a0a4bf8ebd84ab35aaac
Eva Navarrete <evanavarrete07@gmail.com>
2021-06-11 11:37:08 -0600

    Add test suites to rooms files

--- src/Rooms.js
+++ src/Rooms.js
@@ -1,8 +1,14 @@
 class Rooms {
-  constructor(rooms) {
-    this.allRooms = rooms;
-  }
-}
+  constructor(room) {
+    this.number = room.number;
+    this.roomType = room.roomType;
+    this.bidet = room.bidet;
+    this.bedSize = room.bedSize;
+    this.numBeds = room.numBeds;
+    this.costPerNight = room.costPerNight;
+    // console.log('this', this.roomNumber);
+  };
+};
 
 
 
